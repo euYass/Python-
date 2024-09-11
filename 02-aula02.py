74aca020aff957296304ad888c7ac342828d3c04
-Yass <yaslima572@gmail.com>
2024-09-11 14:37:57 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# Atribuição de variáveis
+nome = "Yasmim"
+idade = "17"
+altura = "1.61"
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade: ", idade, "altura: ", idade ,)
+#Formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
