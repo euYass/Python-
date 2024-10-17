e02aacaa5effd7a6033cf4c889d1a1665c7d7833
Yass ;) <yaslima572@gmail.com>
2024-10-17 17:56:11 -0300

    Add files via upload

---
+++ 22-numero-1a100.py
@@ -0,0 +1,4 @@
+print("CONTANDO DE 1 A 100\n")
+
+for i in range(1, 101):
+    print(i)
---
+++ 23-tabuada-for.py
@@ -0,0 +1,6 @@
+print("TABUADA\n")
+      
+numero = int(input("Digite um número: "))
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
